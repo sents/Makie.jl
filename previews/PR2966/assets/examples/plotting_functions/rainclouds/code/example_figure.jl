5b86eb61afb92038e9ec1126f326f8ec38e500c3
Documenter.jl <documenter@juliadocs.github.io>
2023-07-14 13:12:03 +0000

    build based on bf55d8b

---
+++ previews/PR2966/assets/examples/plotting_functions/rainclouds/code/example_figure.jl
@@ -0,0 +1,12 @@
+# This file was generated, do not modify it. # hide
+__result = begin # hide
+    category_labels, data_array = mockup_categories_and_data_array(6)
+rainclouds(category_labels, data_array;
+    xlabel = "Categories of Distributions",
+    ylabel = "Samples", title = "My Title",
+    plot_boxplots = true, cloud_width=0.5,
+    color = colors[indexin(category_labels, unique(category_labels))])
+end # hide
+save(joinpath(@OUTPUT, "example_4858774243463866941.png"), __result; ) # hide
+
+nothing # hide
